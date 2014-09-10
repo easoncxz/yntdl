e3e7df771a4980634e782149a8c2f24f0a3fd892
Eason Chen <easoncxz@gmail.com>
2014-09-11 01:58:28 +1200

    Added a JSON marshaller (in servlet-context.xml), created a dummy domain object (Thing.java), and demonstrated side-by-side JSP rendering and domain object marshalling.

---
+++ src/main/java/com/easoncxz/yntdl/domain/Thing.java
@@ -0,0 +1,15 @@
+package com.easoncxz.yntdl.domain;
+
+public class Thing {
+
+	private String label;
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public String getLabel() {
+		return this.label;
+	}
+
+}
