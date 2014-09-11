66d7224b24110fc8a0bcc069fbefa24e3010aacb
Eason Chen <easoncxz@gmail.com>
2014-09-11 17:08:17 +1200

    Created some testing tools.

---
+++ src/test/java/com/easoncxz/ntdl/TestDaoJdbc.java
@@ -0,0 +1,30 @@
+package com.easoncxz.ntdl;
+
+import java.util.List;
+
+import org.springframework.util.Assert;
+
+import com.easoncxz.yntdl.domain.User;
+import com.easoncxz.yntdl.persistence.Dao;
+
+public class TestDaoJdbc extends TestTemplate {
+
+	public static void main(String[] args) {
+		Dao dao = TestTemplate.context.getBean("daoJdbcTemplate", Dao.class);
+		Assert.notNull(dao);
+
+		boolean flag = false;
+		if (flag) {
+			User u = new User();
+			u.setHumanReadableName("John Doe");
+			dao.saveUser(u);
+			Assert.notEmpty(dao.getAllUsers());
+		}
+
+		List<User> users = dao.getAllUsers();
+		Assert.notNull(users);
+		for (User u : users) {
+			System.out.println(u.getHumanReadableName());
+		}
+	}
+}
---
+++ src/test/java/com/easoncxz/ntdl/TestTemplate.java
@@ -0,0 +1,10 @@
+package com.easoncxz.ntdl;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestTemplate {
+	
+	protected static ApplicationContext context = new ClassPathXmlApplicationContext("test-context.xml");
+
+}
