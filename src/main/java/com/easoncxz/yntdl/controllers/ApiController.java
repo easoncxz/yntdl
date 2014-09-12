9e9da9763160f54c1627fd9e80cee13ca9fd0b4a
Eason Chen <easoncxz@gmail.com>
2014-09-12 17:44:06 +1200

    Configured Hibernate stuff in the servlet context. Created the first HTTP response containing a marshalled domain object (an empty List<User>).

--- src/main/java/com/easoncxz/yntdl/controllers/ApiController.java
+++ src/main/java/com/easoncxz/yntdl/controllers/ApiController.java
@@ -2,17 +2,24 @@ package com.easoncxz.yntdl.controllers;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.easoncxz.yntdl.domain.User;
 import com.easoncxz.yntdl.persistence.Dao;
 
 @Controller(value = "/api")
 public class ApiController {
 	
+	@Autowired
 	private Dao dao;
 	
+	@RequestMapping
+	@ResponseBody
 	public List<User> getAllUsers() {
-		
+		return dao.getAllUsers();
 	}
 
 }
