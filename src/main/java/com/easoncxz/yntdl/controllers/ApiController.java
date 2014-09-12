4e6951023f3278b05c0fb3b6f4d8030a930f8e4a
Eason Chen <easoncxz@gmail.com>
2014-09-12 17:29:00 +1200

    Resolved merge conflicts. Nothing seems to be really conflicting.

--- src/main/java/com/easoncxz/yntdl/controllers/ApiController.java
+++ src/main/java/com/easoncxz/yntdl/controllers/ApiController.java
@@ -1,5 +1,18 @@
 package com.easoncxz.yntdl.controllers;
 
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+
+import com.easoncxz.yntdl.persistence.Dao;
+
+@Controller(value = "/api")
 public class ApiController {
+	
+	private Dao dao;
+	
+	public List<User> getAllUsers() {
+		
+	}
 
 }
