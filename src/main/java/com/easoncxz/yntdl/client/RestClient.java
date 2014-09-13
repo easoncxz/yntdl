3026ce4710eca18585676de773ece729096f0116
Eason Chen <easoncxz@gmail.com>
2014-09-13 14:52:59 +1200

    Delete tests; Created client interface.

---
+++ src/main/java/com/easoncxz/yntdl/client/RestClient.java
@@ -0,0 +1,14 @@
+package com.easoncxz.yntdl.client;
+
+import com.easoncxz.yntdl.service.Service;
+
+/**
+ * Just because the client needs to be able to do the same CRUD operations as
+ * the server.
+ * 
+ * @author eason
+ *
+ */
+public interface RestClient extends Service {
+
+}
